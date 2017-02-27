7610385f5abf3a595ad4f016202c4552f8930ab2
saisamrat <sai.samrat36@gmail.com>
2017-02-26 23:19:22 -0500

    Change for anaylitcs

--- popu.js
+++ popu.js
@@ -3,7 +3,18 @@ $(function(){
 	removeNote();
 	clearNotes();
 	downloadNotes();
+	analytics();
 });
+
+function analytics(){
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92674858-1', 'auto');
+  ga('send', 'pageview');
+}
 
 function render(){
 	chrome.storage.sync.get({text:[]}, function(data){
